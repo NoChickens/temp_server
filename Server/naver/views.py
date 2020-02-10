e3df8bb2b7407a6ce1b24e6b00bc567a8dc46e37
Lee Kyunghwan <khrock1@gmail.com>
2020-02-10 22:45:01 +0900

    dayoff

--- Server/naver/views.py
+++ Server/naver/views.py
@@ -8,8 +8,12 @@ import urllib.request
 # from decouple import config
 
 
-# 1. 이미지 업로드 순간(나라 코드까지) - OCR로 보내주는 함수
+# 2. OCR 결과값을 파파고 NMT로 옮겨주는 함수
+def transferring(request):
+    return
 
+
+# 1. 이미지 업로드 순간(나라 코드까지) - OCR로 보내주는 함수
 def sending(request):
     # base64_result = request.data    지수형이 보내 줄 거 
     with open('./OCR.json', 'r', encoding='utf-8') as f:
@@ -32,18 +36,16 @@ def sending(request):
             if key == "images":
                 for k in val[0]["fields"]:
                     empty+=k['inferText']+' '
+
         print(empty)  # 여기에 inferText에 모든 것이 나옴.
-        
         
     else:
         print("Error Code:" + rescode)
 
-        # return Response(temp)    결과 값 inferText 파파고로 보내보셈 알아서 
+    return transferring(empty)nnn
 
 
-# 2. OCR 결과값을 파파고 NMT로 옮겨주는 함수
-def transferring(request):
-    return
+
 
 # 3. 번역 결과 값 띄어주는 함수
 def checking(request):
